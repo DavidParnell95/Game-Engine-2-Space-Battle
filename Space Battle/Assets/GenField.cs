747a90a1b9cfd0e638e2a249dd12a5d4c5472757
DavidParnell95 <55593664+DavidParnell95@users.noreply.github.com>
2020-05-14 19:52:19 +0100

    Space Background and Asteroid generation implemented

---
+++ Space Battle/Assets/GenField.cs
@@ -0,0 +1,37 @@
+﻿/**** GenField ****
+Script to generate an asteroid field
+using a prefab, around an object.
+In this case, an empty game object named:
+Field Center
+*****************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenField : MonoBehaviour
+{
+    public Transform asteroidPref;//Asteroid object
+    public int fieldRadius = 100;
+    public int count = 250;//number of asteroids to generate 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i =0; i < count; i++)
+        {
+            /*
+            Instantiate the asteroid, at a random point around the 
+            center of the field, with a default rotation 
+            */
+            Instantiate(asteroidPref, Random.insideUnitSphere * 
+            fieldRadius, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
