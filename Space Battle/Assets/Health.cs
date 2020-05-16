f8cbc5687a0cbdd85414e027f9479f31e2355deb
DavidParnell95 <55593664+DavidParnell95@users.noreply.github.com>
2020-05-16 11:54:09 +0100

    Health stats implemented

---
+++ Space Battle/Assets/Health.cs
@@ -0,0 +1,25 @@
+﻿//Script to handle enemy health
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float health = 100f;//Starting health
+
+    //When recieve damage, take off from health
+    public void TakeDamage(float amount)
+    {
+        health -= amount; 
+
+        //Check if health is 0 or less
+        if(health <= 0f)
+        {
+            //if so die
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);//Destroy the object
+    }
+}
