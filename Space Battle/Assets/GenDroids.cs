a91a4e02c84c0cfaf127f581119e2bd7e0282d0f
DavidParnell95 <55593664+DavidParnell95@users.noreply.github.com>
2020-05-16 10:15:55 +0100

    Spawn droids

---
+++ Space Battle/Assets/GenDroids.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenDroids : MonoBehaviour
+{
+    public Transform droidPref;//Asteroid object
+    public int fieldRadius = 100;
+    public int count = 20;//number of asteroids to generate 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i =0; i < count; i++)
+        {
+            /*
+            Instantiate the asteroid, at a random point around the 
+            center of the field, with a random rotation 
+            */
+            Transform tempDroid =Instantiate(droidPref, Random.insideUnitSphere * 
+            fieldRadius, Random.rotation);
+
+            //Set as child of AsteroidField, makes menue cleaner
+            tempDroid.transform.parent = GameObject.Find("Droids").transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
