455f6d5a36bbd2b3e507b7e72dd27f356fe72e02
DavidParnell95 <55593664+DavidParnell95@users.noreply.github.com>
2020-05-18 12:32:17 +0100

    Cohesion smoothing and boundaries implemented
    Changed cohesion to smooth animations. Behavior to stop droids leaving the asteroid field implemented

---
+++ Space Battle/Assets/Scripts/Droid/Behaviours/StayInRadius.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="Flock/Behavior/Stay In Radius")]
+public class StayInRadius : FlockBehavior
+{
+    GameObject fieldCenter;
+    Vector3 center;
+    public float radius = 50f;
+    
+    public override Vector3 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        fieldCenter  = GameObject.Find("FieldCenter");
+        Debug.Log(fieldCenter.ToString());
+        Transform fieldTransform = fieldCenter.transform;
+        center = fieldTransform.position;
+
+        Vector3 centerOffset = center - agent.transform.position;
+        float t = centerOffset.magnitude / radius;
+
+        //If not too far, no adjustment needed
+        if(t < .9f)
+        {
+            return Vector3.zero;
+        }
+
+        return centerOffset * t * t; 
+    }
+
+}
