0151a491b9baedc7a326070978a15fa1c9147d0b
DavidParnell95 <55593664+DavidParnell95@users.noreply.github.com>
2020-05-18 11:33:51 +0100

    Combine Steering behaviors

---
+++ Space Battle/Assets/Scripts/Droid/Behaviours/CompositeBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behavior/Composite")]
+public class CompositeBehavior : FlockBehavior
+{
+    public FlockBehavior[] behaviors;
+    public float[] weights;
+
+    public override Vector3 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        //Check if same number of weights as behaviors
+        if(weights.Length != behaviors.Length)
+        {
+            Debug.LogError("Data mismatch: " +name,this);
+            return Vector3.zero;
+        }
+
+        //Set up move
+        Vector3 move = Vector3.zero;
+
+        //iterate through behaviors
+        for(int i = 0; i< behaviors.Length; i++)
+        {
+            Vector3 partMove = behaviors[i].CalculateMove(agent,context,flock) * weights[i];
+
+            //If some movement
+            if(partMove != Vector3.zero)
+            {
+                if(partMove.sqrMagnitude > weights[i] * weights[i])
+                {
+                    partMove.Normalize();
+                    partMove *= weights[i];
+                }
+
+                move += partMove;
+            }
+        }
+
+        return move;
+    }
+}
---
+++ Space Battle/Assets/Scripts/Droid/Behaviours/SteeredCohesion.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behavior/SteeredCohesion")]
+public class SteeredCohesion : FlockBehavior
+{
+    Vector3 currentVel;
+    public float smoothTime = .5f;//time taken to smooth velocity
+
+  //Implement Calculate move so it can inherit from FlockBehavior
+    public override Vector3 CalculateMove(
+        FlockAgent agent, List<Transform> context, 
+        Flock flock)
+    {
+        //If no neighbours, no adjustment
+        if(context.Count == 0)
+        {
+            return Vector3.zero;
+        }
+
+        //Add context members and avg out
+        Vector3 cohesionMove = Vector3.zero;
+            
+        //iterate through members and add
+        foreach(Transform item in context)
+        {
+            cohesionMove += item.position;
+        }
+
+        //Get avg
+        cohesionMove /= context.Count;
+
+        //Create offset from agent pos
+        cohesionMove -= agent.transform.position;
+        cohesionMove = Vector3.SmoothDamp(agent.transform.forward, cohesionMove, 
+        ref currentVel, smoothTime);
+
+        return cohesionMove;
+
+    }
+
+}
